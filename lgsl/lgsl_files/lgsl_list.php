a179055f2fc60940756213191b6d9e982b3a5ec2
Neon <neon1226@yandex.ru>
2019-09-25 11:54:01 +0400

    Added more css classes

--- lgsl/lgsl_files/lgsl_list.php
+++ lgsl/lgsl_files/lgsl_list.php
@@ -18,62 +18,68 @@
 //------------------------------------------------------------------------------------------------------------+
 
   $output .= "
-  <div style='text-align:center;margin-top:30px;'>
-    <table id='server_list_table' cellpadding='4' cellspacing='2'>";
+	<table id='server_list_table'>
+		<tr id='server_list_table_top'>
+			<th>{$lgsl_config['text']['sts']}</th>
+			<th>{$lgsl_config['text']['adr']}</th>
+			<th>{$lgsl_config['text']['tns']}</th>
+			<th>{$lgsl_config['text']['map']}</th>
+			<th>{$lgsl_config['text']['plr']}</th>
+			<th>{$lgsl_config['text']['vsd']}</th>
+		</tr>";
 
-    foreach ($server_list as $server)
-    {
-      $misc   = lgsl_server_misc($server);
-      $server = lgsl_server_html($server);
+	foreach ($server_list as $server)
+	{
+		$misc   = lgsl_server_misc($server);
+		$server = lgsl_server_html($server);
 
-      $output .= "
-      <tr class='server_{$misc['text_status']}'>
+		$output .= "
+		<tr class='server_{$misc['text_status']}'>
 
-        <td style='white-space:nowrap; text-align:center'>
-          <span title='{$lgsl_config['text'][$misc['text_status']]}' class='status_icon_{$misc['text_status']}'></span>
-          <img alt='' src='{$misc['icon_game']}'   title='{$misc['text_type_game']}' class='game_icon' />
-        </td>
+			<td class='status_cell'>
+				<span title='{$lgsl_config['text'][$misc['text_status']]}' class='status_icon_{$misc['text_status']}'></span>
+				<img alt='{$misc['name_filtered']}' src='{$misc['icon_game']}' title='{$misc['text_type_game']}' class='game_icon' />
+			</td>
 
-        <td title='{$lgsl_config['text']['slk']}' style='text-align:right'>
-          <a href='{$misc['software_link']}'>
-            {$server['b']['ip']}:{$server['b']['c_port']}
-          </a>
-        </td>
+			<td title='{$lgsl_config['text']['slk']}' class='connectlink_cell'>
+				<a href='{$misc['software_link']}'>
+					{$server['b']['ip']}:{$server['b']['c_port']}
+				</a>
+			</td>
 
-        <td title='{$server['s']['name']}' style='text-align:left'>
-          <div style='width:100%; overflow:hidden; height:1.3em; line-height:1.3em'>
-            {$misc['name_filtered']}
-          </div>
-        </td>
+			<td title='{$server['s']['name']}' class='servername_cell'>
+				<div>
+					{$misc['name_filtered']}
+				</div>
+			</td>
 
-        <td style='white-space:nowrap; text-align:left'>
-          {$server['s']['map']}
-        </td>
+			<td class='map_cell'>
+				{$server['s']['map']}
+			</td>
 
-        <td style='white-space:nowrap; text-align:right'>
-          {$server['s']['players']} / {$server['s']['playersmax']}
-        </td>
+			<td class='players_cell'>
+				{$server['s']['players']} / <span class='maxplayers'>{$server['s']['playersmax']}</span>
+			</td>
 
-        <td style='white-space:nowrap; text-align:center'>";
+			<td class='details_cell'>";
 
-        if ($lgsl_config['locations'])
-        {
-          $output .= "
-          <a href='".lgsl_location_link($server['o']['location'])."' style='text-decoration:none'>
-            <img alt='' src='{$misc['icon_location']}' title='{$misc['text_location']}' class='contry_icon' />
-          </a>";
-        }
+			if ($lgsl_config['locations'])
+			{
+				$output .= "
+				<a href='".lgsl_location_link($server['o']['location'])."'>
+					<img alt='' src='{$misc['icon_location']}' title='{$misc['text_location']}' class='contry_icon' />
+				</a>";
+			}
 
-        $output .= "
-          <a href='".lgsl_link($server['o']['id'])."' class='details_icon' title='{$lgsl_config['text']['vsd']}'></a>
-        </td>
+			$output .= "
+				<a href='".lgsl_link($server['o']['id'])."' class='details_icon' title='{$lgsl_config['text']['vsd']}'></a>
+			</td>
 
-      </tr>";
-    }
+		</tr>";
+	}
 
-    $output .= "
-    </table>
-  </div>";
+	$output .= "
+	</table>";
 
 //------------------------------------------------------------------------------------------------------------+
 
