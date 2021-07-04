782c5d788f1ba1ba005d5863522f59eca00f95d8
Yusuf <36341134+izmoxkawaii@users.noreply.github.com>
2021-07-04 07:42:17 -0700

    Turkish language support

---
+++ lgsl_files/languages/turkish.php
@@ -0,0 +1,83 @@
+<?php
+
+//------------------------------------------------------------------------------------------------------------+
+
+  global $lgsl_config;
+
+  $lgsl_config['text']['ttl'] = "Canlı Oyun Sunucusu Listesi";
+  $lgsl_config['text']['vsd'] = "SUNUCU DETAYLARINI GÖRMEK İÇİN TIKLAYINIZ";
+  $lgsl_config['text']['slk'] = "OYUN BAĞLANTISI";
+  $lgsl_config['text']['sts'] = "Durum";
+  $lgsl_config['text']['adr'] = "Adres";
+  $lgsl_config['text']['cpt'] = "Bağlantı Noktası";
+  $lgsl_config['text']['qpt'] = "Sorgu Bağlantı Noktası";
+  $lgsl_config['text']['typ'] = "Türü";
+  $lgsl_config['text']['gme'] = "Oyun";
+  $lgsl_config['text']['map'] = "Harita";
+  $lgsl_config['text']['plr'] = "Oyuncular";
+  $lgsl_config['text']['dtl'] = "Detaylar";
+  $lgsl_config['text']['npi'] = "OYUNCU BİLGİSİ YOK";
+  $lgsl_config['text']['nei'] = "EKSTRA BİLGİ YOK";
+  $lgsl_config['text']['ehs'] = "Ayar";
+  $lgsl_config['text']['ehv'] = "Değer";
+  $lgsl_config['text']['onl'] = "AKTİF";
+  $lgsl_config['text']['onp'] = "AKTİF FAKAT ŞİFRELİ";
+  $lgsl_config['text']['nrs'] = "VERİ YOK";
+  $lgsl_config['text']['pen'] = "SORGU BEKLENİYOR";
+  $lgsl_config['text']['zpl'] = "OYUNCULAR:";
+  $lgsl_config['text']['mid'] = "GEÇERSİZ SUNUCU KİMLİĞİ";
+  $lgsl_config['text']['nnm'] = "--";
+  $lgsl_config['text']['nmp'] = "--";
+  $lgsl_config['text']['tns'] = "Sunucular";
+  $lgsl_config['text']['tnp'] = "Oyuncular";
+  $lgsl_config['text']['tmp'] = "Azami. Oyuncu Sayısı";
+  $lgsl_config['text']['asd'] = "SUNUCULARIN ORTAK EKLENMESİ DEVRE DIŞI";
+  $lgsl_config['text']['awm'] = "BU ALAN TEST ETMENİZE VE SONRA ÇEVRİMİÇİ OYUN SUNUCULARINI LİSTEYE EKLEMEYE İZİN VERİR";
+  $lgsl_config['text']['ats'] = "Test Sunucusu";
+  $lgsl_config['text']['aaa'] = "SUNUCU ZATEN EKLENDİ VE YÖNETİCİ ONAYI GEREKİYOR";
+  $lgsl_config['text']['aan'] = "SUNUCU ZATEN EKLENDİ";
+  $lgsl_config['text']['anr'] = "YANIT YOK - DOĞRU DETAYLARI GİRDİĞİNİZDEN EMİN OLUN";
+  $lgsl_config['text']['ada'] = "YÖNETİCİ ONAYI İÇİN SUNUCU EKLENDİ";
+  $lgsl_config['text']['adn'] = "SUNUCU EKLENDİ";
+  $lgsl_config['text']['asc'] = "BAŞARI - LÜTFEN DOĞRU SUNUCUSU ONAYLANMASINI BEKLEYİN";
+  $lgsl_config['text']['aas'] = "Sunucuyu Ekle";
+  $lgsl_config['text']['loc'] = "Lokasyon:";
+  $lgsl_config['text']['cts'] = "Sunucu Tanıtım Görsellerini göstermek için tıklayın";
+  $lgsl_config['text']['lst'] = "Son Güncelleme";
+  $lgsl_config['text']['nam'] = "Adı";
+  $lgsl_config['text']['gam'] = "Oyun";
+  $lgsl_config['text']['sta'] = "Durum";
+  $lgsl_config['text']['sco'] = "Skor";
+  $lgsl_config['text']['tim'] = "Zaman";
+  $lgsl_config['text']['tea'] = "Takım";
+  $lgsl_config['text']['cou'] = "Ülke";
+  $lgsl_config['text']['pin'] = "Gecikme";
+  $lgsl_config['text']['pag'] = "Sayfa";
+  $lgsl_config['text']['bak'] = "SUNUCU LİSTESİNE GERİ DÖN";
+  $lgsl_config['text']['mpg'] = "ANA SAYFA'YA DÖN";
+  /* admin section */
+  $lgsl_config['text']['tzn'] = "m/d/Y h:i:sA";
+  $lgsl_config['text']['umn'] = "KULLANICI ADI";
+  $lgsl_config['text']['pwd'] = "ŞİFRE";
+  $lgsl_config['text']['lgn'] = "Giriş Yap";
+  $lgsl_config['text']['skc'] = "Kaydet - Önbelleği Koru";
+  $lgsl_config['text']['srh'] = "Kaydet - Önbelleği Sıfırla";
+  $lgsl_config['text']['mip'] = "Harita Görüntü Yolları";
+  $lgsl_config['text']['avm'] = "Gelişmiş Yönetim";
+  $lgsl_config['text']['nrm'] = "Normal Yönetim";
+  $lgsl_config['text']['upd'] = "Güncellemeler";
+  $lgsl_config['text']['yes'] = "EVET";
+  $lgsl_config['text']['nno'] = "HAYIR";
+  $lgsl_config['text']['enb'] = "Etkin";
+  $lgsl_config['text']['dsb'] = "Devre Dışı";
+  $lgsl_config['text']['aum'] = "YÖNETİCİ KULLANICI ADI VEYA ŞİFRE YAPILANDIRMADAN EKSİK";
+  $lgsl_config['text']['apc'] = "LÜTFEN VARSAYILAN ŞİFRENİZİ DEĞİŞTİRİNİZ";
+  $lgsl_config['text']['faq'] = "- Bir sunucuyu kaldırmak için IP'yi silin ve ardından Kaydet'e tıklayın.<br /><br />- LGSL'nin sizin için doldurmaya çalışması için sorgu bağlantı noktasını boş bırakın.<br /><br />- Yazılım portu sadece birkaç oyun için gereklidir, bu nedenle 0 olarak ayarlanması normaldir.<br /><br />- Stili ve diğer seçenekleri ayarlamak için lgsl_config.php dosyasını düzenleyin.<br /><br />- LGSL'nizi güncel tutun.";
+  $lgsl_config['text']['fso'] = "Sunucuların doğrudan sorgulanması için gerekli";
+  $lgsl_config['text']['crl'] = "fsockopen devre dışı bırakıldığında besleme için kullanılır";
+  $lgsl_config['text']['mbs'] = "UTF-8 sunucu ve oyuncu adlarını doğru göstermek için kullanılır";
+  $lgsl_config['text']['bz2'] = "Belirli bir boyutun üzerindeki Kaynak sunucu ayarlarını göstermek için kullanılır";
+  $lgsl_config['text']['gd2'] = "Görüntü Modu için gerekli";
+  $lgsl_config['text']['zli'] = "America's Army 3 için gerekli";
+
+//------------------------------------------------------------------------------------------------------------+
