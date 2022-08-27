a9e948d9d19d520a4b34ad07646751b6f192482b
Neon <neon1226@yandex.ru>
2022-08-28 00:48:15 +0400

    Details sort script: dynamically sorting players and options

---
+++ lgsl_files/scripts/details-sort.js
@@ -0,0 +1,18 @@
+function loadDetailsSort() {
+    const getSort = ({ target }) => {
+        const order = (target.dataset.order = -(target.dataset.order || -1));
+        const index = [...target.parentNode.cells].indexOf(target);
+        const collator = new Intl.Collator(['ar', 'bg', 'cs', 'de', 'en', 'es', 'fr', 'ko', 'ro', 'ru', 'sk', 'tr', 'zh'], { numeric: true });
+        const comparator = (index, order) => (a, b) => order * collator.compare(
+            a.children[index].innerHTML,
+            b.children[index].innerHTML
+        );
+        for(const tBody of target.closest('table').tBodies)
+            tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+        for(const cell of target.parentNode.cells)
+            cell.classList.toggle('sorted', cell === target);
+    };
+    document.querySelectorAll('.players_table .table_head, .settings_table .table_head').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
+}
+
+document.addEventListener("DOMContentLoaded", loadDetailsSort);
